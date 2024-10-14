018a180752f6d00e44685475581f621004b40f9d
Patryk Karwowski <vb.karwowski@gmail.com>
2024-10-14 12:06:28 +0100

    added expenses api and hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import useExpenses from './api/useExpenses'
+
 function App() {
-  return <main className="m-auto min-h-screen max-w-5xl p-10 bg-gray-100" />;
+  const { loading, error, expenses } = useExpenses()
+  console.log({ loading, error, expenses })
+
+  return (
+    <main className="m-auto min-h-screen max-w-5xl p-10 bg-gray-100">
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+    </main>
+  )
 }
 
-export default App;
+export default App
